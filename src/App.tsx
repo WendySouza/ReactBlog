16bbaf4f952b5e5214f720e9b5fcd73cd3262f15
Wendy Souza <contato.wendy@hotmail.com>
2024-08-06 19:55:36 -0300

    Componentes NavBar, Footer e Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import "./App.css";
 import Home from "./paginas/home/Home";
+import NavBar from "./components/navBar/NavBar";
+import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./paginas/login/Login";
+
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
+        <NavBar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
